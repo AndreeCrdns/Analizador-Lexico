b949286a9000c3352e17f44231460af40920607a
AndreeCrdns <127163475+AndreeCrdns@users.noreply.github.com>
2023-03-08 21:50:30 -0800

    Delete Programa.java

--- Programa.java
+++
@@ -1,8 +0,0 @@
-public class Programa {
-    public static void main(String[] args) throws Exception {
-        Lista ls = new Lista();
-        ls.Llenarlista("C:/Users/andre/OneDrive/Desktop/Leer.txt");
-        System.out.println(ls.validarCadena("variables:"));
-
-    }
-}
